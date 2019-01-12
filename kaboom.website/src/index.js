156015003abecd9f0bc5b7ac3a8646a7e9bbfe19
artemkv <artemkv@gmail.com>
2019-01-12 19:21:28 +0100

    postpone starting app until google gapi is loaded

--- kaboom.website/src/index.js
+++ kaboom.website/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Spinner from "./components/Spinner";
 import "./kaboom.css";
 
-ReactDOM.render(<App />, document.getElementById('react_app'));
+// Wait for the google auth2 client to initialize properly
+// Google auth2 client uses globals, so we must too
+global.kaboom_start = function () {
+    ReactDOM.render(<App />, document.getElementById('react_app'));
+}
+
+// Render spinner while waiting
+ReactDOM.render(<Spinner />, document.getElementById('react_app'));
